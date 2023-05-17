45d82847c16b9deb95dcdae0126100ce30e78c6e
Sasha <131606731+sasha2234562@users.noreply.github.com>
2023-05-17 16:47:12 +0300

    slider

--- Untitled-3.js
+++ Untitled-3.js
@@ -1,55 +1,56 @@
 
-const one = document.querySelector('.one')
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
 
-document.addEventListener('click', fankRaione )
-
-function fankRaione(event){
-  if(event.target.closest('.spanRayon')){
-   one.classList.remove('one');
+document.querySelector('.Left').addEventListener('click', function() {
+  if(offset == 768){
+    offset = -256;
   }
-  else  if(!event.target.closest('.spanRayon')){
-    one.classList.add('one')
-   }
-}
-
-const two = document.querySelector('.two');
-document.addEventListener('click', fankMetro )
-
-function fankMetro(event){
-  if(event.target.closest('.spanRayonTwo')){
-   two.classList.remove('two')
-  }
-  else  if(!event.target.closest('.spanRayonTwo')){
-    two.classList.add('two')
-   }
-}
-
-const three = document.querySelector('.three');
-
-document.addEventListener('click', function(event){
-  if(event.target.closest('.spanRayonThree')){
-    three.classList.remove('three');
-   three.style.transition = "1s"
-  }
-  else if(!event.target.closest('.spanRayonThree')){
-    three.classList.add('three');
-  }
+  offset = offset + 256;
+  sliderLine.style.left = -offset + 'px';
 })
 
-const sdorove = document.querySelector('.menu__list');
+document.querySelector('.raite').addEventListener('click', function() {
+  if(offset == 0){
+    offset = 1024;
+  }
+  offset = offset - 256;
+  sliderLine.style.left = -offset + 'px';
+})
+/*
+const range = document.querySelector('input[type="range"]');
+const number = document.querySelector('.number');
+range.value = 0;
+range.max = images.length - 1;
 
+img.src = './images/' + images[0];
 
-document.addEventListener('click', menu)
-
-function menu (event){
-    if(event.target.closest('.menu__button')){
-        sdorove.classList.remove('_none');
-        sdorove.classList.toggle('_active');
-        sdorove.classList.remove('menu__list')
-    }
-    if(!event.target.closest('.menu__button')){
-        sdorove.classList.remove('_active');
-        sdorove.classList.toggle('_none');
-        sdorove.classList.add('menu__list')
-    }
+range.oninput = () =>{
+  img.src = './images/' + images[range.value];
+  number.innerHTML = range.value;
 }
+
+/*
+const images = [
+  'IMG_8418.jpg',
+  'IMG_8424.jpg',
+  'IMG_8604.jpg',
+  'IMG_20201010_182811.jpg'
+]
+const img = document.querySelector('.img');
+const raite = document.querySelector('.raite');
+const left = document.querySelector('.Left');
+
+//img.src = './images/' + images[i];
+//img.src = './images' + images[0];
+const imagesArr = images.length;
+
+raite.addEventListener('click', ()=>{
+  for (let i = 0; i < images.length; i++) {
+    img.src = './images/' + images[i];
+    
+  }
+  
+
+})
+*/
