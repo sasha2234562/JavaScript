07f744384efbeb39199bf3c043d101d98bea8cba
Sasha <131606731+sasha2234562@users.noreply.github.com>
2023-05-23 21:57:19 +0300

    array

--- Untitled-3.js
+++ Untitled-3.js
@@ -1,55 +1,94 @@
 
-const one = document.querySelector('.one')
+const images = [
+  'crab.png',
+  'dog.png',
+  'frog.png',
+  'giraffe.png',
+  'mouse.png',
+  'pinguin.png',
+  'ryno.png',
+  'seastar.png',
+  'snake.png',
+  'worm.png',
+]
+const one = document.querySelector('.one');
+const img = document.querySelector('.img');
+const input = document.querySelector('.input');
+input.value = 0;
+img = img + images[0];
 
-document.addEventListener('click', fankRaione )
 
-function fankRaione(event){
-  if(event.target.closest('.spanRayon')){
-   one.classList.remove('one');
-  }
-  else  if(!event.target.closest('.spanRayon')){
-    one.classList.add('one')
-   }
+
+
+const buttonTwo = document.querySelector('.buttonTwo');
+
+document.addEventListener('click', (event) =>{
+  if(event.target.closest('.button')){
+  buttonTwo.style.display = "block";
 }
-
-const two = document.querySelector('.two');
-document.addEventListener('click', fankMetro )
-
-function fankMetro(event){
-  if(event.target.closest('.spanRayonTwo')){
-   two.classList.remove('two')
-  }
-  else  if(!event.target.closest('.spanRayonTwo')){
-    two.classList.add('two')
-   }
-}
-
-const three = document.querySelector('.three');
-
-document.addEventListener('click', function(event){
-  if(event.target.closest('.spanRayonThree')){
-    three.classList.remove('three');
-   three.style.transition = "1s"
-  }
-  else if(!event.target.closest('.spanRayonThree')){
-    three.classList.add('three');
+  else if(!event.target.closest('.button')){
+    buttonTwo.style.display = "none"
   }
 })
 
-const sdorove = document.querySelector('.menu__list');
+const inpurButton = document.querySelector('.inpurButton');
+const buttonTreeA = document.querySelector('.buttonTreeA');
 
+document.addEventListener('click', dasha)
 
-document.addEventListener('click', menu)
-
-function menu (event){
-    if(event.target.closest('.menu__button')){
-        sdorove.classList.remove('_none');
-        sdorove.classList.toggle('_active');
-        sdorove.classList.remove('menu__list')
-    }
-    if(!event.target.closest('.menu__button')){
-        sdorove.classList.remove('_active');
-        sdorove.classList.toggle('_none');
-        sdorove.classList.add('menu__list')
-    }
+function dasha (event) {
+  if(event.target.closest('.inpurButton')){
+  buttonTreeA.style.display = "block";
 }
+else if(!event.target.closest('.inpurButton')){
+ buttonTreeA.style.display = "none";
+}}
+
+
+let mashka = 'hello, hi, mahai';
+let dashka = mashka.split(',');
+console.log(dashka);
+
+
+const clients = [
+  {"Petrov" : "user", "debt" : 42},
+  {"Ivanov" : "user", "debt"  : 113},
+  {"Sidorov" : "user", "debt"  : 5},
+  {"Begynec" : "user", "debt"  : 74},
+  {"Kalitsenya" : "user", "debt"  : 0},
+];
+
+let debts = clients.filter(item => (item.debt >= 5)).sort((a,b) =>(b.debt - a.debt));
+
+const text = document.querySelector('.text');
+const inputButton = document.querySelector('.inputButton')
+const divInput = document.querySelector('.divInput')
+document.addEventListener('click', textInput )
+
+function textInput (event){
+  if(event.target.closest('.inputButton')){
+    
+    divInput.innerHTML = `<img src="./images/snake.png" type="png">`;
+
+    //divInput.innerHTML = text.value;
+
+  } else if (!event.target.closest('.inputButton')){
+    divInput.innerHTML = text.value
+  }
+    }
+
+  
+    const button = document.querySelector('.button');
+
+    const form = document.querySelector('#form');
+
+    document.addEventListener('click', sasha)
+    function sasha(event){
+      if(event.target.closest('button')){
+         if(form.elements.inputOne.value.length < 4){
+        console.log('sasha')}
+      }
+      event.preventDefault();
+    }
+     
+    
